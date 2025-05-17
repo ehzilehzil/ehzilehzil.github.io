7bfc9a86b79096ff497f1f6a7500741a228ccfc0
ehzhilehzil <ehzilehzil@outlook.com>
2025-05-17 20:29:46 +0900

    f

---
+++ _markdown/page/[0010]index.md
@@ -0,0 +1,11 @@
+---
+layout: "page"
+title: "인덱스 페이지"
+description: "인덱스 페이지 테스트"
+updated: "2025-05-13"
+tags: ["ehzilehzil", "blog", "builder", "test", "index"]
+---
+
+## 인덱스
+
+인덱스 페이지
--- main.js
+++ main.js
@@ -2,22 +2,41 @@
 
 import fs from "fs-extra";
 import fg from "fast-glob";
+import path from "node:path";
 import * as $ from "./utils.js";
 
 
-/**
- * 페이지(포스팅)에 해당하는 마크다운 파싱 후 임시저장
- */
-{
-    let mdFiles = 
-}
-
-
-/**
- * 테스트
- * @type {() => void}
- */
-let test = () => {
-    let mdFiles = fg.globSync(`./_markdown/**/*.md`).sort();
-    console.log(mdFiles);
+// globals
+let globals = {
+    site: {
+        title: `어질어질 블로그`,
+    },
 };
+
+/**
+ * 마크다운 페이지 정보 파일
+ */
+let pagesInfo = {};
+
+
+// ./_markdown/page/**/*.md ->
+{
+    let mdfiles = fg.globSync(`./_markdown/page/**/*.md`).sort();
+    for (let mdfile of mdfiles) {
+        let vars = {
+            ...globals,
+        };
+
+        let {dir, base, name, ext} = path.parse(mdfile);
+        dir = dir.replace(`./_markdown/page`, ``).replace(/^\//, ``);
+        name = name.replace(/^\[.*\]/, ``);
+        // console.log(`${dir}, ${name}`);
+
+        let permalink = `/page/${name}`;
+        if (permalink === `/page/index`) permalink = `/`;
+        let jsonfile = `./_site/page/${name}.json`;
+
+        let {data, content} = $.parse.separate(fs.readFileSync(mdfile, {encoding: `utf-8`}));
+        
+    }
+} 
